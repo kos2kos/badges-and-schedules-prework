cd1e6d05a188478c153c9c081036295e7c458b43
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-11 02:40:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
 # Write your code here.
+def badge_maker(str)
+  return "Hello, my name is #{str}."
+end
+
+def batch_badge_creator (arr)
+  temp = []
+  arr.each do |names|
+    temp << badge_maker(arr)
+  end
+end
+
+def assign_rooms(speakers)
+  rooms = []
+  speakers.each_with_index {|item, index| 
+    rooms[index]= "Hello, #{item}! You'll be assigned to room #{index}!"}
+  puts rooms
+  return rooms 
+end
