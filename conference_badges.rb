16e4eba61aae99d2f8f55b6ed72006a69594b2dc
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-11 03:01:04 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(speakers)
   rooms = []
   speakers.each_with_index {|item, index| 
-    rooms[index]= "Hello, #{item}! You'll be assigned to room #{index}!"}
+    rooms[index]= "Hello, #{item}! You'll be assigned to room #{index.to_i + 1}!"}
   puts rooms
   return rooms 
 end
